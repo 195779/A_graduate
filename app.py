01f42cc5af94787e5a5408b5903507fe7b429398
Alan <alancym.wjr@gmail.com>
2023-01-26 14:15:59 +0800

    Add index page

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from flask import render_template
 
 @app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
 @app.route('/index')
 @app.route('/home')
 def hello():
@@ -46,3 +51,17 @@ def test_number(number):
 
 
     #以 . 开头的文件默认会被隐藏，执行ls命令看不见他们，需要执行ls -f 
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
